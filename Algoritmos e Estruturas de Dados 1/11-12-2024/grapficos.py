d459a4a8801d783f2ffce2e341509db39f9d0c7b
Caetano Gomes <164023589+celricae@users.noreply.github.com>
2024-12-11 15:03:07 +0000

    correção

--- Algoritmos e Estruturas de Dados 1/11-12-2024/grapficos.py
+++ Algoritmos e Estruturas de Dados 1/11-12-2024/grapficos.py
@@ -8,7 +8,16 @@ import graphics as gf
 win = gf.GraphWin("Minha Janela", 400, 350)
 
 c = gf.Circle(gf.Point(100,150), 10)
+cores = ['red', 'blue', 'green', 'yellow', 'black', 'white', 'purple', 'orange', 'pink', 'brown']
 c.setFill("red")
 c.draw(win)
-onde_cliquei = win.getMouse()
-print(onde_cliquei.getX(), onde_cliquei.getY())
+cont = 0
+while True:
+    onde_cliquei = win.getMouse()
+    print(onde_cliquei.getX(), onde_cliquei.getY())
+    nova_bolinha = gf.Circle(onde_cliquei, 10)
+    nova_bolinha.setFill(cores[cont])
+    cont += 1
+    if cont == len(cores):
+        cont = 0
+    nova_bolinha.draw(win)
